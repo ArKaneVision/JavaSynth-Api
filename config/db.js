6be1314467f1006765faea385d726777e814faaa
Matt Shirley <me@mattshirley.net>
2020-05-21 19:59:13 -0700

    dockerize app

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongooseBaseName = 'bitcube-image'
 
 // create the mongodb uri for development and test
 const database = {
-  development: `mongodb://localhost/${mongooseBaseName}-development`,
+  development: process.env.DB || `mongodb://localhost/${mongooseBaseName}-development`,
   test: `mongodb://localhost/${mongooseBaseName}-test`
 }
 
